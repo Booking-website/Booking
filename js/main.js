4e1bed13c9437eb128b86ac0c94e46ca88595e9c
1August <boktaban@gmail.com>
2021-05-20 01:17:48 +0600

    profile page, credit card rotating

--- js/main.js
+++ js/main.js
@@ -6,7 +6,13 @@ $(document).ready(function() {
   $(".goTopBtn").click(function() {
     $('html, body').animate({scrollTop:0},'500');
     // window.scrollTo({top: 0, behavior: "smooth"});
-  })
+  });
+
+  $(".main__bank-card p").click(function() {
+    $(".main__bank-card i").toggleClass("rotate180");
+    $(".main__bank-card-dropdown").toggleClass("show");
+  });
+
 
 
 
@@ -37,86 +43,3 @@ function set_min_date() {
     });
   });
 };
-
-// function toggle_bcc_ways(element) {
-//   if (element.hasClass("active")) {
-//     element.attr("style", "background-color: #d7e7f2");
-//   } else {
-//     element.attr("style", "background-color: transparent");
-//   }
-// };
-
-// $("#multi_city").on("click", function() {
-//   $(this).addClass("active");
-//   $(".multi-city").show();
-//   $("#roundtrip").removeClass("active");
-//   $(".roundtrip").hide();
-//   $("#one_way").removeClass("active");
-//   $(".one-way").hide();
-//   var date_from = $("#date-from");;
-//   date_from.on("click", function(){
-//     var today = new Date();
-//     var dd = today.getDate();
-//     var mm = today.getMonth()+1;
-//     var yyyy = today.getFullYear();
-//      if(dd<10){
-//             dd='0'+dd
-//         }
-//         if(mm<10){
-//             mm='0'+mm
-//         }
-//     today = yyyy+'-'+mm+'-'+dd;
-//     $(".multi-city input[type='date']").each(function (i, el) {
-//       el.setAttribute("min", today);
-//       // $(".multi-city input").val(set_date);
-//     });
-//     // document.getElementById('date-from').setAttribute("min", today);
-//     // document.getElementById('date-to').setAttribute("min", today);
-//   });
-// });
-// $(".multi-city input").each(function(i, el){
-//   $(".multi-city input").on("change", function(){
-//     var set_date = $(".multi-city input").val();
-//     $(".multi-city input[type='date']").each(function (i, el) {
-//       $(".multi-city input").val(set_date);
-//     });
-//   });
-// });
-
-
-// $("#logo").on("hover", function() {
-//   $("#logo").setAttribute("src", "../img/Booking-red.png");
-// });
-
-
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * max);
-// }
-// var rand_pos = getRandomInt(9);
-// if (rand_pos == 0) {
-//   $("#qr_code").css("background-position", "left top");
-// }else if (rand_pos == 1) {
-//   $("#qr_code").css("background-position", "center top");
-// }else if (rand_pos == 2) {
-//   $("#qr_code").css("background-position", "right top");
-// }else if (rand_pos == 3) {
-//   $("#qr_code").css("background-position", "left center");
-// }else if (rand_pos == 4) {
-//   $("#qr_code").css("background-position", "center center");
-// }else if (rand_pos == 5) {
-//   $("#qr_code").css("background-position", "right center");
-// }else if (rand_pos == 6) {
-//   $("#qr_code").css("background-position", "left bottom");
-// }else if (rand_pos == 7) {
-//   $("#qr_code").css("background-position", "center bottom");
-// }else if (rand_pos == 8) {
-//   $("#qr_code").css("background-position", "right bottom");
-// }
-// $(".goTopBtn").click(function() {
-//   window.scrollTo({top: 0, behavior: "smooth"}, "slow");
-// })
-
-
-// $(".goTopBtn").click(function() {
-//   window.scrollTo({top: 0, behavior: "smooth"}, "slow");
-// })
--- profile.php
+++ profile.php
@@ -8,34 +8,124 @@
 </head>
   <main class="main" id="main">
     <div class="container">
-      <div class="info">
-        <div class="info__left">
+      <div class="main__info">
+
+
+
+
+        <div class="main__sidebar">
+          <ul>
+            <li>
+              <a href="index.php">
+                <i class="fa fa-plane" aria-hidden="true"></i>
+                <span>Book Flight Tickets</span>
+              </a>
+            </li>
+            <li>
+              <a href="view.booked.tickets.php">
+                <i class="fa fa-plane" aria-hidden="true"></i>
+                <span>View Booked Flight Tickets</span>
+              </a>
+            </li>
+            <li>
+              <a href="cancel.booked.tickets.php">
+                <i class="fa fa-plane" aria-hidden="true"></i>
+                <span>Cancel Booked Flight Tickets</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="main__profile-info">
+          <div class="main__profile-img-wrapper">
+            <div class="main__profile-img">
+              <img src="./img/alone.jpg" alt="profile image"/>
+            </div>
+          </div>
+          <div class="main__individual-info">
+            <?php
+            include_once './includes/dbh.inc.php';
+            $query="SELECT count(*), fname, lname, email, phoneNumber FROM users WHERE userID=?";
+            $stmt=mysqli_prepare($conn,$query);
+            mysqli_stmt_bind_param($stmt,"s",$_SESSION['userid']);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt,$cnt,$fname,$lname,$email,$phoneNumber);
+            mysqli_stmt_fetch($stmt);
+            if($cnt==1)
+            {
+              echo "<h4 style='padding-left: 20px;'>".$fname. " " .$lname."</h4><br>";
+              echo "<h4 style='padding-left: 20px;'>".$phoneNumber."</h4><br>";
+              echo "<h4 style='padding-left: 20px;'>".$email."</h4><br>";
+            }
+            mysqli_stmt_close($stmt);
+            mysqli_close($conn);
+            ?>
+          </div>
+          <div class="main__bank-card">
+            <p>My bank card <i class="fa fa-sort-down"></i></p>
+          <div class="main__bank-card-dropdown">
+            <div class="wrap">
+              <div class="card">
+                <div class="front">
+                  <form class="bank-card" action="" method="post">
+                    <div class="card-number">
+                      <input type="number" class="card-number card-number1" name="card-number1" value="" maxlength="4" min="1000" max="9999" placeholder="XXXX">
+                      <input type="number" class="card-number card-number2"  name="card-number2" value="" maxlength="4" min="1000" max="9999" placeholder="XXXX">
+                      <input type="number" class="card-number card-number3"  name="card-number3" value="" maxlength="4" min="1000" max="9999" placeholder="XXXX">
+                      <input type="number" class="card-number card-number4"  name="card-number4" value="" maxlength="4" min="1000" max="9999" placeholder="XXXX">
+                    </div>
+                    <div class="card-date">
+                      <span class="valid">Valid thru</span>
+                      <input type="number" class="card-date card-date1" name="card-date1" maxlength="2" min="01" max="12" value="" placeholder="MM">
+                      <span>/</span>
+                      <input type="number" class="card-date card-date2" name="card-date2" maxlength="2" min="10" value="" placeholder="YY">
+                    </div>
+                    <div class="name-surname">
+                      <input type="text" name="name" value="" placeholder="Name" maxlength="15">
+                      <input type="text" name="surname" value="" placeholder="Surname" maxlength="15">
+                    </div>
+                  </form>
+                  <a href="#" class="flipToBack">></a>
+                </div>
+                <div class="back">
+                  <form class="bank-card" action="" method="post">
+                    <div class="card-cvv">
+                      <input type="number" class="card-cvv" name="card-cvv" value="" placeholder="CVV">
+                    </div>
+                  </form>
+                  <a href="#" class="flipToFront">></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+
+
+
+        <!-- /////////////////////////////////////////////////////////////////////// -->
+        <!-- <div class="info__left">
           <div class="info__profile-photo">
             <img src="https://via.placeholder.com/300x300.jpg/BCB1C3/000000?hello=%20"/>
           </div>
-          <!-- <div class="breakline">
-            <h5>text</h5>
-            <hr>
-          </div> -->
           <div class="info__profile-info">
             <h3 class="name-surname">
               <?php
-              include_once './includes/dbh.inc.php';
-              $query="SELECT count(*), fname, lname, email, phoneNumber FROM users WHERE userID=?";
-        			$stmt=mysqli_prepare($conn,$query);
-        			mysqli_stmt_bind_param($stmt,"s",$_SESSION['userid']);
-        			mysqli_stmt_execute($stmt);
-        			mysqli_stmt_bind_result($stmt,$cnt,$fname,$lname,$email,$phoneNumber);
-        			mysqli_stmt_fetch($stmt);
-        			if($cnt==1)
-        			{
-        				echo "<h4 style='padding-left: 20px;'> NAME: ".$fname."</h4><br>";
-                echo "<h4 style='padding-left: 20px;'> LAST NAME: ".$lname."</h4><br>";
-                echo "<h4 style='padding-left: 20px;'> EMAIL: ".$email."</h4><br>";
-                echo "<h4 style='padding-left: 20px;'> PHONE NUMBER: ".$phoneNumber."</h4><br>";
-        			}
-        			mysqli_stmt_close($stmt);
-        			mysqli_close($conn);
+              // include_once './includes/dbh.inc.php';
+              // $query="SELECT count(*), fname, lname, email, phoneNumber FROM users WHERE userID=?";
+        			// $stmt=mysqli_prepare($conn,$query);
+        			// mysqli_stmt_bind_param($stmt,"s",$_SESSION['userid']);
+        			// mysqli_stmt_execute($stmt);
+        			// mysqli_stmt_bind_result($stmt,$cnt,$fname,$lname,$email,$phoneNumber);
+        			// mysqli_stmt_fetch($stmt);
+        			// if($cnt==1)
+        			// {
+        			// 	echo "<h4 style='padding-left: 20px;'> NAME: ".$fname."</h4><br>";
+              //   echo "<h4 style='padding-left: 20px;'> LAST NAME: ".$lname."</h4><br>";
+              //   echo "<h4 style='padding-left: 20px;'> EMAIL: ".$email."</h4><br>";
+              //   echo "<h4 style='padding-left: 20px;'> PHONE NUMBER: ".$phoneNumber."</h4><br>";
+        			// }
+        			// mysqli_stmt_close($stmt);
+        			// mysqli_close($conn);
               ?>
             </h3>
           </div>
@@ -74,8 +164,8 @@
               </div>
             </div>
           </div>
-        </div>
-        <h3>AVAILABLE ACTIONS:</h3>
+        </div> -->
+        <!-- <h3>AVAILABLE ACTIONS:</h3>
         <table cellpadding="5">
     			<tr>
     				<td><a href="index.php"><i class="fa fa-plane" aria-hidden="true"></i> Book Flight Tickets</a>
@@ -89,7 +179,13 @@
     				<td><a href="cancel.booked.tickets.php"><i class="fa fa-plane" aria-hidden="true"></i> Cancel Booked Flight Tickets</a>
     				</td>
     			</tr>
-    		</table>
+    		</table> -->
+                <!-- /////////////////////////////////////////////////////////////////////// -->
+
+
+
+
+
       </div>
     </div>
   </main>
