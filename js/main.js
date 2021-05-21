e85a1ff40fc5530b53260260d343863a2f951ab1
1August <boktaban@gmail.com>
2021-05-22 01:58:26 +0600

    responsibilty(22.05)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Scroll Reveal
-  ScrollReveal().reveal('.flights, .card1, .card2, .card3, .card4, .p5_card', { delay: 1 });
+  ScrollReveal().reveal('.flights, .part1, .part2, .part3, .part4, .part5', { delay: 100 });
 
 
   $(".goTopBtn").click(function() {
